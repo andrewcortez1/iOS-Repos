307632a100e263422f7d02e8e6b9c798a25bd23b
andrewcortez1 <93861759+andrewcortez1@users.noreply.github.com>
2022-03-13 13:08:22 +0700

    iOS Layoutting

---
+++ IamRichv2/IamRichv2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IamRichv2
+//
+//  Created by IOS on 13/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
