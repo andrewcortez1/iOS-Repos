ed9edc601b91bd108424e3b1044fb1acb07cbd23
andrewcortez1 <93861759+andrewcortez1@users.noreply.github.com>
2022-03-16 23:10:20 +0700

    Add files via upload

---
+++ guessdice/guessdice/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  guessdice
+//
+//  Created by IOS on 16/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func button_tebak(_ sender: UITextField) {
+    }
+    @IBOutlet weak var textView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
